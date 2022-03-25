7d7976f36b0c7e041d27e9e27e0ab70792f61610
Lorena Córdova <lorenacg2107@gmail.com>
2022-03-25 09:47:13 -0400

    Se añadió soporte a cuentas de usuarios y roles, y soporte a base de datos, así como también un tema para la aplicación.

--- laravel_demo/routes/web.php
+++ laravel_demo/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//Agregamos los controladores
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function(){
+    Route::resource('roles', RolController::class);
+    Route::resource('usuarios', UsuarioController::class);
+    Route::resource('vehiculos', VehiculoController::class);
 });
